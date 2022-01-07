07030639b781b5e1e889f7395380e3c7a36267f2
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-01-07 11:40:42 -0800

    feat: v1 html, js, and css

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const h2 = document.createElement('h2');
-h2.textContent = "Welcome to my website!";
-document.querySelector('body').appendChild(h2);
+// const h2 = document.createElement('h2');
+// h2.textContent = "Welcome to my website!";
+// document.querySelector('body').appendChild(h2);
+
+//TODO: FIX ME link about, resume, and contacts in nav with additional pages
+const aboutMe = document.getElementbyId('about');
+function clickAlert() {
+    alert('Page coming soon!');
+}
+aboutMe.addEventListener('click', clickAlert);
